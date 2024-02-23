5335cb2026f6122845c9f91cdb995f1ea241d835
Vadim <cybersnuff@yandex.ru>
2024-02-23 21:52:01 +0400

    commit

--- src/main/java/app/model/Timer.java
+++ src/main/java/app/model/Timer.java
@@ -1,8 +1,19 @@
 package app.model;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+@Component
+
 public class Timer {
 
+    //    @Autowired
+//    @Qualifier("animalsCage")
+//    private AnimalsCage animalsCage;
     private Long nanoTime = System.nanoTime();
+
 
     public Long getTime() {
         return nanoTime;
